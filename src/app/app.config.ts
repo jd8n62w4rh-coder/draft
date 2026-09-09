29285504472ba331c9a3a14b28e462d09cf36626
soph <soph@Sophies-MacBook-Air-7.local>
2026-09-09 18:18:01 -0400

    fix: add hash routing fallback for github pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,17 @@
+// import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideBrowserGlobalErrorListeners(),
+//     provideZonelessChangeDetection(),
+//     provideRouter(routes)
+//   ]
+// };
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router'; // 👈 1. Added withHashLocation here
 
 import { routes } from './app.routes';
 
@@ -7,6 +19,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes, withHashLocation()) // 👈 2. Added inside the parenthesis here
   ]
 };
